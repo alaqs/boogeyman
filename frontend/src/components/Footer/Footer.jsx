12fc775fb876c82f6fe9228f4d078db1bc856c51
Ali Maqsood <alii.maqsood95@gmail.com>
2023-09-10 15:53:44 +0100

    feat: add saqib email on footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -6,9 +6,9 @@ export const Footer = () => {
       <Grid container direction={"row"} alignItems={"center"}
             sx={{width: "100%", backgroundColor: '#4f7375', minHeight: 60, paddingX: 24}}>
         <Typography variant={"body1"} sx={{color: "white"}}>
-          Or, straight give us a call at &nbsp;
-          <a href={"tel:+447564755316"} style={{color: "inherit", fontWeight: 700}}>
-            +44 75647 55316
+          Or, straight send us an email at &nbsp;
+          <a href={"mailto:saqib@techmelon.uk"} style={{color: "inherit", fontWeight: 700}}>
+            saqib@techmelon.uk
           </a>
         </Typography>
       </Grid>
