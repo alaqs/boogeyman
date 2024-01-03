93ab298f6147fa2dd38739e14966113f06a54f7f
Ali Maqsood <alii.maqsood95@gmail.com>
2024-01-03 09:13:06 +0000

    update email

--- frontend/src/components/EmailPrompt.jsx
+++ frontend/src/components/EmailPrompt.jsx
@@ -2,12 +2,12 @@ export const EmailPrompt = () => (
   <span>
     Email kinda human? Hit us straight at{" "}
     <a
-      href={"mailto:saqib@techmelon.uk"}
+      href={"mailto:info@techmelon.uk"}
       style={{ color: "inherit", fontWeight: 700 }}
       target="_blank"
       rel="noreferrer"
     >
-      saqib@techmelon.uk
+      info@techmelon.uk
     </a>
   </span>
 );
